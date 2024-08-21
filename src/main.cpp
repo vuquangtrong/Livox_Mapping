45be4c5be5845e6247add9ed1a2e3604f85a01c8
vuquangtrong <vuquangtrong@gmail.com>
2024-08-21 20:18:40 +0700

    Connect to lidar and get state

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <mutex>
 #include <condition_variable>
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,45 +10,54 @@
 #include <livox_def.h>
 #include <livox_sdk.h>
 
-DeviceInfo lidar_device;
+uint8_t lidar_handle;
 std::mutex lidar_mutex;
 std::condition_variable lidar_connected;
+std::condition_variable lidar_sampling;
 
-/* Callback functin when sample data received */
-void OnSampleDataReceived(uint8_t handle, LivoxEthPacket *data, uint32_t data_num, void *client_data)
-{
-    if (data)
-    {
-    }
-}
-
-/* Callback function when broadcast message received. */
 void OnDeviceBroadcastReceived(const BroadcastDeviceInfo *info)
 {
-    // only connect to the first
+    // only connect to Mid-40 lidar
     if (info == nullptr || info->dev_type != kDeviceTypeLidarMid40)
     {
         return;
     }
-    printf("Receive Broadcast Code: %s,  Device Type: kDeviceTypeLidarMid40\n", info->broadcast_code);
 
-    /* add */
-    livox_status ret = AddLidarToConnect(info->broadcast_code, &lidar_device.handle);
+    printf("Receive Broadcast Code: %s\n", info->broadcast_code);
+    livox_status ret = AddLidarToConnect(info->broadcast_code, &lidar_handle);
     if (ret == kStatusSuccess)
     {
-        SetDataCallback(lidar_device.handle, OnSampleDataReceived, nullptr);
-        lidar_device.state = kLidarStateUnknown;
+        printf("Assigned to handle: %u\n", lidar_handle);
     }
 }
 
-void OnCommonCommandResonsed(livox_status status, uint8_t handle, uint8_t response, void *client_data)
+void OnSampleDataReceived(uint8_t handle, LivoxEthPacket *data, uint32_t data_num, void *client_data)
 {
+}
+
+void OnLidarErrorReceived(livox_status status, uint8_t handle, ErrorMessage *message)
+{
+    printf("=== ERROR ===\n");
+    printf("temp_status: %u\n", message->lidar_error_code.temp_status);
+    printf("volt_status: %u\n", message->lidar_error_code.volt_status);
+    printf("motor_status: %u\n", message->lidar_error_code.motor_status);
+    printf("dirty_warn: %u\n", message->lidar_error_code.dirty_warn);
+    printf("firmware_err: %u\n", message->lidar_error_code.firmware_err);
+    printf("pps_status: %u\n", message->lidar_error_code.device_status);
+    printf("fan_status: %u\n", message->lidar_error_code.fan_status);
+    printf("self_heating: %u\n", message->lidar_error_code.self_heating);
+    printf("ptp_status: %u\n", message->lidar_error_code.ptp_status);
+    printf("time_sync_status: %u\n", message->lidar_error_code.time_sync_status);
+    printf("system_status: %u\n", message->lidar_error_code.system_status);
+    printf("=============\n");
 }
 
 void OnLidarConnected(const DeviceInfo *info)
 {
-    LidarSetMode(info->handle, kLidarModeNormal, OnCommonCommandResonsed, nullptr);
-    lidar_connected.notify_one();
+    SetErrorMessageCallback(info->handle, OnLidarErrorReceived);
+    printf("Request lidar power on\n");
+    LidarSetMode(info->handle, kLidarModeNormal, nullptr, nullptr);
+    lidar_connected.notify_all();
 }
 
 void OnLidarDisconnected(const DeviceInfo *info)
@@ -63,6 +73,9 @@ void OnLidarStateChanged(const DeviceInfo *info)
         break;
     case kLidarModeNormal:
         printf("OnLidarStateChanged kLidarModeNormal\n");
+        SetDataCallback(lidar_handle, OnSampleDataReceived, nullptr);
+        printf("Start sampling...\n");
+        LidarStartSampling(lidar_handle, nullptr, nullptr);
         break;
     case kLidarStatePowerSaving:
         printf("OnLidarStateChanged kLidarStatePowerSaving\n");
@@ -79,15 +92,15 @@ void OnLidarStateChanged(const DeviceInfo *info)
     }
 }
 
-/* Callback function when device status changed */
-void OnDeviceInfoChanged(const DeviceInfo *info, DeviceEvent event)
+void OnDeviceInfoChanged(const DeviceInfo *info, DeviceEvent type)
 {
-    if (info == nullptr || info->handle != lidar_device.handle)
+    // only process registered Mid-40 lidar
+    if (info == nullptr || info->handle != lidar_handle)
     {
         return;
     }
 
-    switch (event)
+    switch (type)
     {
     case kEventConnect:
         printf("OnDeviceInfoChanged event: kEventConnect\n");
@@ -102,7 +115,7 @@ void OnDeviceInfoChanged(const DeviceInfo *info, DeviceEvent event)
         OnLidarStateChanged(info);
         break;
     default:
-        printf("OnDeviceInfoChanged event: %d\n", event);
+        printf("OnDeviceInfoChanged event: %d\n", type);
         break;
     }
 }
@@ -115,37 +128,43 @@ void WaitForDevicesReady()
 
 void ProcessData()
 {
-    uint8_t delay = 30;
-    while (delay)
-    {
-        sleep(1);
-        delay--;
-        printf("[%d]\n", delay);
-    }
+    std::unique_lock<std::mutex> lock(lidar_mutex);
+    lidar_sampling.wait(lock);
 }
 
-void Shutdown()
+void PrepareShutdown()
 {
-    uint8_t delay = 10;
-    while (delay)
-    {
-        sleep(1);
-        delay--;
-        printf("[%d]\n", delay);
-    }
+    printf("Request lidar power off\n");
+    LidarSetMode(lidar_handle, kLidarModePowerSaving, nullptr, nullptr);
+
+    sleep(10);
 }
 
+void ctr_c_handler(int s)
+{
+    printf("Caught signal %d\n", s);
+    lidar_sampling.notify_all();
+}
 
 int main(int argc, const char *argv[])
 {
-    setbuf(stdout, NULL);
+    /* OPTIONS */
+    // setbuf(stdout, NULL);
     // setvbuf(stdout, NULL, _IONBF, 0);
 
+    /* EXCEPTION */
+    struct sigaction _sigaction;
+    _sigaction.sa_handler = ctr_c_handler;
+    sigemptyset(&_sigaction.sa_mask);
+    _sigaction.sa_flags = 0;
+    sigaction(SIGINT, &_sigaction, NULL);
+
+    /* MAIN */
     printf("Livox Mapping 0.1\n");
-    DisableConsoleLogger();
 
     /* INIT */
     printf("SDK initializing...\n");
+    DisableConsoleLogger();
     if (!Init())
     {
         return -1;
@@ -156,10 +175,10 @@ int main(int argc, const char *argv[])
     GetLivoxSdkVersion(&_sdkversion);
     printf("SDK version %d.%d.%d\n", _sdkversion.major, _sdkversion.minor, _sdkversion.patch);
 
-    /* REGISTER CALLBACKS */
+    /* REGISTER GLOBAL CALLBACKS */
     SetBroadcastCallback(OnDeviceBroadcastReceived);
     SetDeviceStateUpdateCallback(OnDeviceInfoChanged);
-    printf("Callback registered.\n");
+    printf("Global Callbacks registered.\n");
 
     /* START */
     if (!Start())
@@ -170,17 +189,19 @@ int main(int argc, const char *argv[])
 
     /* WAIT */
     WaitForDevicesReady();
+    printf("Device connected\n");
 
     /* PROCESS DATA */
+    printf("Start processing...\n");
     ProcessData();
 
-    /* DONE */
-    LidarSetMode(lidar_device.handle, kLidarModePowerSaving, OnCommonCommandResonsed, nullptr);
+    /* WORK DONE */
+    printf("Preparing for shutdown...\n");
+    PrepareShutdown();
 
     /* EXIT */
 __exit:
-    printf("Shuting down...\n");
-    Shutdown();
+    printf("Shutting down...\n");
     Uninit();
     printf("Livox SDK UNinitialized.\n");
     return 0;
